e46b53ecc99d1c7d159ac12cf38518eb178c5b66
subhash kumar <guptasubhash77117@gmail.com>
2023-05-21 22:47:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("sahi se chal rha hai n");
+const hmiburger=document.getElementById('hamBargar');
+const hideLink=document.getElementById('hiddnLinks');
+const croXX=document.getElementById('croxx');
+
+hmiburger.addEventListener('click',()=>{
+    hideLink.style.scale=1;
+    croXX.style.scale=1;
+   
+});
